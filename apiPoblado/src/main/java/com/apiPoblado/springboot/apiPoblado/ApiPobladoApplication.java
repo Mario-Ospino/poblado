2d26499f503a3cf2095bcd3b7330c8f56966cae2
Mario-Ospino <maanosro@gmail.com>
2021-08-24 10:20:24 -0500

    curso fundamentos de spring platzi

---
+++ apiPoblado/src/main/java/com/apiPoblado/springboot/apiPoblado/ApiPobladoApplication.java
@@ -0,0 +1,13 @@
+package com.apiPoblado.springboot.apiPoblado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPobladoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPobladoApplication.class, args);
+	}
+
+}
---
+++ apiPoblado/src/test/java/com/apiPoblado/springboot/apiPoblado/ApiPobladoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiPoblado.springboot.apiPoblado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPobladoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
