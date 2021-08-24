c7a0adf538646a8cf8cb70a3d96a01176c923eca
Mario-Ospino <maanosro@gmail.com>
2021-08-24 01:55:51 -0500

    añadimos service

--- pobladoweb/src/app/core/services/users.service.ts
+++ pobladoweb/src/app/core/services/users.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http' 
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  user: Users[];
+  constructor( private http: HttpClient) { }
+  URL = 'http://localhost:8080/api/users';
 
-  constructor() { }
+  getUsers(){
+    return this.http.get<user>
+  }
 }
