84f2edc5d4d93cd0764251cbfaa177b782b79bdc
Mario-Ospino <maanosro@gmail.com>
2021-08-23 22:26:51 -0500

    header terminado

--- pobladoweb/src/app/app-routing.module.ts
+++ pobladoweb/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',
+  component: LayoutComponent,
+  pathMatch: 'full',
+  children: [
+    {path:'',loadChildren: () => import('./home/home.module').then( m => m.HomeModule)},
+  ]
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
